13e1fd28ad06e93c156a40b47b13cc5f395a2d2c
George Marzloff <3322166+gmarzloff@users.noreply.github.com>
2022-08-15 14:04:59 -0600

    requirements.txt added

--- main.py
+++ main.py
@@ -15,39 +15,42 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        animating_label = QLabel('animation here')
-        animating_label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
-        animating_label.pixmap().size().setWidth(200)
-        animating_label.pixmap().size().setHeight(200)
-        self.setCentralWidget(animating_label)
+        self.animating_label = QLabel('animation here')
+        self.animating_label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.animating_label.pixmap().size().setWidth(200)
+        self.animating_label.pixmap().size().setHeight(200)
+        self.setCentralWidget(self.animating_label)
 
         parent_path = Path(__file__).parent
         self.images_path = parent_path / 'images/animation_set/'
-        animating_label.setPixmap(QPixmap(self.images_path.__str__() + "/clock01.png"))
-
+        self.animating_label.setPixmap(QPixmap(self.images_path.__str__() + "/clock01.png"))
 
         self.pixmapcache_keys: [str] = []
 
-        def load_images(self):
-            image_suffixes = ['.jpg', '.png', '.gif', '.bmp']
-            imgs_found_count = 0
-            for filepath in self.images_path.iterdir():
-                if filepath.suffix in image_suffixes:
-                    imgs_found_count += 1
-                    self.pixmap().load(filepath.__str__())
-                    cache_key = filepath.stem
-                    self.pixmapcache_keys.append(cache_key)
-                    QPixmapCache.insert(cache_key, self.pixmap())
-                    # except:
-                    #     print(cache_key, "pixmap not added to QPixmapCache.")
-            print(imgs_found_count, "image(s) found in animation_set directory.", len(self.pixmapcache_keys),
-                  "keys loaded into QPixmapCache")
+        self.load_images()
+        test = QPixmapCache.find("clock02")
+        self.animating_label.setPixmap(test)
+        # self.animating_label.setPixmap(QPixmapCache.find("clock03"))
+        pass
 
-        # mock
-        # QPixmapCache.insert('manual', self.pixmap())
-        # default_pixmap = QPixmapCache.find("manual")
-        # self.setPixmap(default_pixmap)
+    def load_images(self):
+        image_suffixes = ['.jpg', '.png', '.gif', '.bmp']
+        imgs_found_count = 0
+        for filepath in self.images_path.iterdir():
+            if filepath.suffix in image_suffixes:
+                imgs_found_count += 1
+                cache_key = filepath.stem
+                self.pixmapcache_keys.append(cache_key)
 
+                if not QPixmapCache.find(cache_key):
+                    pixmap = QPixmap(filepath.__str__())
+                    QPixmapCache.insert(cache_key, pixmap)
+
+                print("pixmap %s" % cache_key, QPixmapCache.find(cache_key))
+
+
+        print(imgs_found_count, "image(s) found in animation_set directory.", len(self.pixmapcache_keys),
+              "keys loaded into QPixmapCache")
 
 app = QApplication(sys.argv)
 
