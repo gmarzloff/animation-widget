e56a0c0b9f62aa69060d72c47eedd6371282b67a
George Marzloff <3322166+gmarzloff@users.noreply.github.com>
2022-08-30 22:55:20 -0600

    aesthetic improvement to slider, cleaned up code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PyQt6.QtWidgets import (
     QSlider
 )
 from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtGui import QPixmap, QPixmapCache
+from PyQt6.QtGui import QPixmap
 from pathlib import Path
 
 class MainWindow(QMainWindow):
@@ -22,30 +22,34 @@ class MainWindow(QMainWindow):
         self.animating_label.pixmap().size().setHeight(200)
 
         self.frame_rate_label = QLabel('0')
+        self.frame_rate_label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        font = self.frame_rate_label.font()
+        font.setPointSizeF(18)
+        font.setBold(True)
+        self.frame_rate_label.setFont(font)
 
         rate_slider = QSlider()
         rate_slider.setMinimum(1)
         rate_slider.setMaximum(50)
-        rate_slider.setContentsMargins(120, 120, 120, 120)
+        rate_slider.setFixedWidth(80)
         rate_slider.valueChanged.connect(self.slider_changed)
 
-        self.left_layout = QVBoxLayout()
-        self.left_layout.addWidget(self.frame_rate_label)
-        self.left_layout.addWidget(rate_slider)
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.frame_rate_label)
+        left_layout.addWidget(rate_slider)
 
-        self.layout = QHBoxLayout()
-        self.layout.addLayout(self.left_layout)
-        self.layout.addWidget(self.animating_label)
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(left_layout)
+        main_layout.addWidget(self.animating_label)
 
         widget = QWidget()
-        widget.setLayout(self.layout)
+        widget.setLayout(main_layout)
         self.setCentralWidget(widget)
 
         parent_path = Path(__file__).parent
         self.images_path = parent_path / 'images/animation_set/'
         self.animating_label.setPixmap(QPixmap(self.images_path.__str__() + "/clock01.png"))
 
-        self.animation_keys: [str] = []
         self.pixmaps: [QPixmap] = []
         self.pixmap_current_index = -1
         self.load_images()
@@ -63,14 +67,8 @@ class MainWindow(QMainWindow):
         for filepath in sorted(self.images_path.iterdir()):
             if filepath.suffix in image_suffixes:
                 imgs_found_count += 1
-                cache_key = filepath.stem
-                self.animation_keys.append(cache_key)
-
                 qpixmap = QPixmap(filepath.__str__())
                 self.pixmaps.append(qpixmap)
-
-        print(imgs_found_count, "image(s) found in animation_set directory.", len(self.animation_keys),
-              "keys loaded into QPixmapCache")
 
     def advance(self):
         if self.pixmap_current_index >= len(self.pixmaps) - 1:
@@ -88,6 +86,7 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 
 window = MainWindow()
+window.setWindowTitle("Control frame rate of animation example")
 window.show()
 
 # start Qt event loop
